170b1403afdad1a48227d4f2f1c528a2faa0e843
Blood Marillion <bmarilli@er-d3.kzn.21-school.ru>
2021-02-16 22:33:17 +0300

    change .h

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -17,8 +17,24 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+/*
+** IS
+*/
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_isspace(int c);
+
+/*
+** MEM
+*/
+
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
+void				*ft_calloc(size_t count, size_t size);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
@@ -26,40 +42,16 @@ void				*ft_memchr(const void *s, int c, size_t n);
 void				ft_memdel(void **ap);
 void				*ft_memalloc(size_t size);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
-int					ft_print_n_char(int num, char c, int fd);
-size_t				ft_strlen(const char *str);
-size_t				ft_strlcat(char *dst, const char *src, size_t size);
-size_t				ft_strlcpy(char *dst, const char *src, size_t size);
-size_t				ft_intlen(long long n);
-char				*ft_strnew(size_t size);
-char				*ft_strchr(const char *s, int c);
-char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *big, const char *little,
-					size_t len);
-char				*ft_create_str(char c, int n);
-int					ft_add_forward(char **str, char c, int n);
-int					ft_add_back(char **str, char c, int n);
-void				ft_strdel(char **as);
-int					ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/*
+** NUMBERS
+*/
+
 int					ft_atoi(const char *str);
+size_t				ft_intlen(long long n);
 int					ft_atoi_base(char *str, char *base);
 double				ft_atof(char *str);
 void				ft_skip_atof(char *str, int *j);
-int					ft_isalpha(int c);
-int					ft_isdigit(int c);
-int					ft_isalnum(int c);
-int					ft_isascii(int c);
-int					ft_isprint(int c);
-int					ft_toupper(int c);
-int					ft_tolower(int c);
-int					ft_isspace(int c);
-int					ft_pow(int a, int n);
-void				*ft_calloc(size_t count, size_t size);
-char				*ft_substr(char const *s, unsigned int start, size_t len);
-char				*ft_strjoin(char const *s1, char const *s2);
-char				*ft_strtrim(char const *s1, char const *set);
-char				**ft_split(char const *s, char c);
-char				*ft_strdup(const char *s1);
 char				*ft_itoa(int n);
 char				*ft_itoa_ll(long long int n);
 char				*ft_itoa_unsigned_ll(unsigned long long n);
@@ -68,11 +60,53 @@ char				*ft_itoa_unsig_base(unsigned long long value, int base,
 		int num, int reg);
 char				*ft_itoa_base(unsigned long long int value,
 		int base, char c);
-char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int					ft_pow(int a, int n);
+
+/*
+** PUT
+*/
+
+int					ft_print_n_char(int num, char c, int fd);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+
+/*
+** STR
+*/
+
+int					ft_add_forward(char **str, char c, int n);
+int					ft_add_back(char **str, char c, int n);
+char				*ft_create_str(char c, int n);
+size_t				ft_strlen(const char *str);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+char				*ft_strnew(size_t size);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+char				*ft_strnstr(const char *big, const char *little,
+					size_t len);
+void				ft_strdel(char **as);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strtrim(char const *s1, char const *set);
+char				**ft_split(char const *s, char c);
+char				*ft_strdup(const char *s1);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+/*
+** TO
+*/
+
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+
+/*
+** GARBAGE COLLECTOR
+*/
+
 void				ft_garbage_collector(void *ptr, int action);
 void				*calloc_gc(size_t nmemb, size_t size);
 void				*malloc_gc(size_t sizemem);
