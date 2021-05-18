ab8d18a8e27df4726d6d7d918d5fccfb5a980a9a
F@nzil <nabiyarov.fanzil@gmail.com>
2021-05-18 23:08:29 +0300

    Update libft.h

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -20,11 +20,16 @@ extern "C" {
 
 	
 #ifndef COLOR
-	
-# define F_NONE "\033[37m"
-# define RED "\033[31m"
-# define GREEN "\033[32m"
-# define CYANE "\033[36m"
+
+# define COLOR
+# define F_NONE		"\033[37m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define CYANE		"\033[36m"
+# define F_BOLD		"\033[1m"
+# define F_ORANGE	"\033[38m"
+# define F_YELLOW	"\033[0;33m"
+# define F_BLUE		"\033[34m" 
 
 #endif
 	
