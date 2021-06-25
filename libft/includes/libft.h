270f0522fcbea1a2c737d3cdfced7359b7782bc6
Blood Marillion <bmarilli@er-f1.kzn.21-school.ru>
2021-06-25 23:31:47 +0300

    add color

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -31,6 +31,25 @@ extern "C" {
 # define F_YELLOW	"\033[0;33m"
 # define F_BLUE		"\033[34m" 
 
+# define NCLR        "\033[0m"
+# define BLACK       "\033[0;30m"
+# define WHITE       "\033[0;37m"
+# define YELLOW      "\033[0;33m"
+# define RED         "\033[0;31m"
+# define GREEN       "\033[0;32m"
+# define CYAN        "\033[0;36m"
+# define BLUE        "\033[0;34m"
+# define PURPLE      "\033[0;35m"
+# define B_GRAY      "\033[1;30m"
+# define B_WHITE     "\033[1;37m"
+# define B_RED       "\033[1;31m"
+# define B_YELLOW    "\033[1;33m"
+# define B_GREEN     "\033[1;32m"
+# define B_CYAN      "\033[1;36m"
+# define B_BLUE      "\033[1;34m"
+# define B_PURPLE    "\033[1;35m
+
+
 #endif
 	
 # define M_ADD 1
