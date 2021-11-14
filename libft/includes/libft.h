de8eff88d3b4d89d542cae6158150f301cf2fc3d
ifanzilka <nabiyarov.fanzil@gmail.com>
2021-11-14 15:33:40 +0300

    fix bag

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 15:51:44 by bmarilli          #+#    #+#             */
-/*   Updated: 2021/03/27 14:56:39 by bmarilli         ###   ########.fr       */
+/*   Updated: 2021/11/14 15:33:06 by bmarilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,6 @@ extern "C" {
 # define BLACK       "\033[0;30m"
 # define WHITE       "\033[0;37m"
 # define YELLOW      "\033[0;33m"
-# define RED         "\033[0;31m"
-# define GREEN       "\033[0;32m"
 # define CYAN        "\033[0;36m"
 # define BLUE        "\033[0;34m"
 # define PURPLE      "\033[0;35m"
@@ -47,7 +45,7 @@ extern "C" {
 # define B_GREEN     "\033[1;32m"
 # define B_CYAN      "\033[1;36m"
 # define B_BLUE      "\033[1;34m"
-# define B_PURPLE    "\033[1;35m
+# define B_PURPLE    "\033[1;35m"
 
 
 #endif
